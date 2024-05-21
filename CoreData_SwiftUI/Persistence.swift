a319404078e840486454c526056bd9cb759c3510
Tim Mitra <tim@it-guy.com>
2024-05-21 01:50:32 -0400

    changed to UserInfo

--- CoreData_SwiftUI/Persistence.swift
+++ CoreData_SwiftUI/Persistence.swift
@@ -14,8 +14,8 @@ struct PersistenceController {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
         for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let newItem = UserInfo(context: viewContext)
+            //newItem. = Date()
         }
         do {
             try viewContext.save()
