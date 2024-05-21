3430b257ba33726e0b9ec2ffec66a939aa128d29
Tim Mitra <tim@it-guy.com>
2024-05-21 01:51:12 -0400

    update the form

--- CoreData_SwiftUI/UserInfoView.swift
+++ CoreData_SwiftUI/UserInfoView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct UserInfoView: View {
+  @Environment(\.managedObjectContext) private var moc
+  @State private var firstName = ""
+  @State private var lastName = ""
+  @State private var gender = ""
+  @State private var isAlert = false
+
+  private let genders = ["Male", "False"]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      NavigationView{
+        Form {
+          Section {
+            TextField("Enter first name", text: $firstName)
+              .disableAutocorrection(true)
+            TextField("Enter last name", text: $lastName)
+              .disableAutocorrection(true)
+            Picker("Select gender", selection: $gender) {
+              ForEach(genders, id: \.self) { gender in
+                Text(gender)
+              }
+            }
+            Button(action: addItem) {
+              Text("Add User")
+            }
+          }
+        }
+        .navigationBarTitle("User Info View")
+      }
     }
+  
+  private func addItem() {
+      withAnimation {
+          let newItem = UserInfo(context: moc)
+        newItem.firstName = firstName
+        newItem.lastName = lastName
+        newItem.gender = gender
+          do {
+              try moc.save()
+          } catch {
+              // Replace this implementation with code to handle the error appropriately.
+              // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+              let nsError = error as NSError
+              fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+          }
+      }
+  }
 }
 
 struct UserInfoView_Previews: PreviewProvider {
